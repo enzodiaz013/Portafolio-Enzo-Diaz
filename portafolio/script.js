49d7022d846686f132f8bde33ad55313b9d4e65e
Enzo Diaz <114183050+enzodiaz013@users.noreply.github.com>
2023-03-17 21:08:46 -0300

    Add files via upload

---
+++ portafolio/script.js
@@ -0,0 +1,23 @@
+function seleccionar(link){
+    var opciones = document.querySelectorAll('#links a');
+    opciones[0].className="";
+    opciones[1].className="";
+    opciones[2].className="";
+    opciones[3].className="";
+    opciones[4].className="";
+    link.className = "seleccionado";
+
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+function resposiveMenu(){
+    var x = document.getElementById("nav");
+    if (x.className ===""){
+        x.className = "responsive";
+    }
+    else{
+        x.className = "";
+    }
+}
+
